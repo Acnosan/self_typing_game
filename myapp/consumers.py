c18060cfb29dc78fd6872439dfad5eb8e8890515
AcnoHako <153207339+Acnosan@users.noreply.github.com>
2024-05-20 10:59:56 +0100

    works

--- myapp/consumers.py
+++ myapp/consumers.py
@@ -13,6 +13,7 @@ class TypingConsumer(WebsocketConsumer):
         self.accept()
         self.send_new_snippet()
 
+
     def send_new_snippet(self):
         if self.current_snippet_index < len(self.snippets):
             snippet_text = self.snippets[self.current_snippet_index]
@@ -25,19 +26,18 @@ class TypingConsumer(WebsocketConsumer):
             self.send(json.dumps({
                 'game_over': True
             }))
-            
 
     def disconnect(self, close_code):
         pass
 
     def receive(self, text_data):
         data = json.loads(text_data)
-        typed_text = data.get('typed_text', '')
-        snippet_text = data.get('snippet_text', '')
+        typed_text = data.get('typed_text', '').lower()
+        snippet_text = data.get('snippet_text', '').lower()
 
         is_correct = typed_text == snippet_text
         if is_correct:
-            self.current_snippet_index = (self.current_snippet_index + 1) % len(self.snippets)
+            self.current_snippet_index = (self.current_snippet_index + 1)
             self.send_new_snippet()
         else:
             remaining_text = snippet_text[len(typed_text):]
--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+
 # Create your views here.
 def type_game(response):
 
